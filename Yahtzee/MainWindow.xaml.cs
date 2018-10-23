982b7b5cdf9034675230e2076c62bfcc18687408
Huego <hugozaanen@hotmail.com>
2018-10-23 12:33:45 +0200

    YAHTZEE

--- Yahtzee/MainWindow.xaml.cs
+++ Yahtzee/MainWindow.xaml.cs
@@ -18,38 +18,52 @@ namespace Yahtzee
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-        
+
     public partial class MainWindow : Window
     {
         
+        int[] nums = new int[5];
+        int numRolls = 1;
         
         public MainWindow()
         {
             InitializeComponent();
+            txtBox.Text = "" + numRolls;
         }
-
+               
         private void RollBttn_Click(object sender, RoutedEventArgs e)
         {
-            Random rand  = new Random();
-            Random rand1 = new Random();
-            Random rand2 = new Random();
-            Random rand3 = new Random();
-            Random rand4 = new Random();
+            if (numRolls < 3)
+            {
+                numRolls++;
+                txtBox.Text = "" + numRolls;
 
-            int num = rand.Next(1,6);
-            int num1 = rand.Next(1,6);
-            int num2 = rand.Next(1,6);
-            int num3 = rand.Next(1,6);
-            int num4 = rand.Next(1,6);
+                Random rand = new Random();
+                Random rand1 = new Random();
+                Random rand2 = new Random();
+                Random rand3 = new Random();
+                Random rand4 = new Random();
 
-            box1.Text = "" + num;
-            box2.Text = "" + num1;
-            box3.Text = "" + num2;
-            box4.Text = "" + num3;
-            box5.Text = "" + num4;
+                int num = rand.Next(1, 6);
+                int num1 = rand.Next(1, 6);
+                int num2 = rand.Next(1, 6);
+                int num3 = rand.Next(1, 6);
+                int num4 = rand.Next(1, 6);
 
-            
+                nums[0] = num;
+                nums[1] = num1;
+                nums[2] = num2;
+                nums[3] = num3;
+                nums[4] = num4;
+
+                box1.Text = "" + num;
+                box2.Text = "" + num1;
+                box3.Text = "" + num2;
+                box4.Text = "" + num3;
+                box5.Text = "" + num4;
+            }
         }
+    
 
         private void player1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -60,8 +74,70 @@ namespace Yahtzee
             int num3 = Int32.Parse(box2.Text);
             int num4 = Int32.Parse(box2.Text);
             int num5 = Int32.Parse(box2.Text);
+            
+            if(block.Name == "player1" )
+            {
+                block.Text = "" + ScoreCalculator.CountOnes(nums);
+            }
+            else if(block.Name == "player2")
+            {
+                block.Text = "" + ScoreCalculator.CountTwos(nums);
+            }
+            else if(block.Name == "player3")
+            {
+                block.Text = "" + ScoreCalculator.CountThrees(nums);
+            }
+            else if(block.Name == "player4")
+            {
+                block.Text = "" + ScoreCalculator.CountFours(nums);
+            }
+            else if(block.Name == "player5")
+            {
+                block.Text = "" + ScoreCalculator.CountFives(nums);
+            }
+            else if(block.Name == "player6")
+            {
+                block.Text = "" + ScoreCalculator.CountOnes(nums);
+            }
+            else if(block.Name == "TwoOfaKind")
+            {
 
-            block.Text = "" + (num1 + num2 + num3 + num4 + num5);
+            }
+            else if (block.Name == "FourOfaKind")
+            {
+
+            }
+            else if (block.Name == "FullHouse")
+            {
+
+            }
+            else if (block.Name == "SmallStraight")
+            {
+
+            }
+            else if (block.Name == "BigStraight")
+            {
+
+            }
+            else if (block.Name == "Chance")
+            {
+
+            }
+            else if (block.Name == "YAHTZEE")
+            {
+
+            }
+
+            //block.Text = "" + (num1 + num2 + num3 + num4 + num5);
+
+            block.IsEnabled = false;
+             numRolls = 1;
+             txtBox.Text = "" + numRolls;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Array.Sort<int>(nums);
+        }        
     }
 }
---
+++ Yahtzee/ScoreCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class ScoreCalculator
+    {
+
+        public static int CountOnes(int[] array)
+        {
+            return CountOneToSix(array, 1);
+        }
+
+        public static int CountTwos(int[] array)
+        {
+            return CountOneToSix(array, 2);
+        }
+
+        public static int CountThrees(int[] array)
+        {
+            return CountOneToSix(array, 3);
+        }
+
+        public static int CountFours(int[] array)
+        {
+            return CountOneToSix(array, 4);
+        }
+
+        public static int CountFives(int[] array)
+        {
+            return CountOneToSix(array, 5);
+        }
+
+        public static int CountSix(int[] array)
+        {
+            return CountOneToSix(array, 6);
+        }
+
+        
+
+        public static int CountOneToSix(int[] array, int number)
+        {
+            int sum = 0;
+            foreach (int i in array)
+            {
+                if (i == number)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }       
+    }
+}
